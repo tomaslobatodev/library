8bf53d4ff902902e00fbe5034bec3ed7c4b2a23e
Tomas Lobato <tomaslobatodev@gmail.com>
2023-08-28 16:41:57 -0300

    form validation added

--- script.js
+++ script.js
@@ -1,56 +1,71 @@
-const content = document.querySelector('.content')
-const addBtn = document.querySelector('.addBook')
-const modal = document.querySelector('.modal')
-const closeModalBtn = document.querySelector('.closeModal')
-const createBookBtn = document.querySelector('.createBook')
+const content = document.querySelector(".content")
+const addBtn = document.querySelector(".addBook")
+const modal = document.querySelector(".modal")
+const closeModalBtn = document.querySelector(".closeModal")
+const createBookBtn = document.querySelector(".createBook")
 
 let books = []
 
 class Book {
-    constructor(title, author, pages, read) {
-        this.title = title
-        this.author = author
-        this.pages = pages
-        this.read = read
-        this.id = books.length + 1
-        this.info = function() {
-            return `This is ${this.title} by ${this.author}, it has ${this.pages} pages.`
-        }
+  constructor(title, author, pages, read) {
+    this.title = title
+    this.author = author
+    this.pages = pages
+    this.read = read
+    this.id = books.length + 1
+    this.info = function () {
+      return `This is ${this.title} by ${this.author}, it has ${this.pages} pages.`
     }
+  }
 }
 
-
-addBtn.addEventListener('click', () => {
-    modal.style.display = 'flex'
+addBtn.addEventListener("click", () => {
+  modal.style.display = "flex"
 })
 
-closeModalBtn.addEventListener('click', () => {
-    modal.style.display = 'none'
+closeModalBtn.addEventListener("click", () => {
+  modal.style.display = "none"
 })
 
 createBookBtn.addEventListener('click', (ev) => {
-    ev.preventDefault()
-    const titleInput = document.querySelector('#title')
-    const authorInput = document.querySelector('#author')
-    const pagesInput = document.querySelector('#pages')
+  const titleInput = document.querySelector('#title');
+  const authorInput = document.querySelector('#author');
+  const pagesInput = document.querySelector('#pages');
+  const errorMessage = document.querySelector('#errormessage');
 
-    const title = titleInput.value
-    const author = authorInput.value
-    const pages = pagesInput.value
+  const title = titleInput.value;
+  const author = authorInput.value;
+  const pages = pagesInput.value;
 
-    const newBook = new Book(title, author, pages, false)
-    books.push(newBook)
+  if (title.length < 3 || title.length > 50) {
+    return;
+  }
 
-    titleInput.value = ''
-    authorInput.value = ''
-    pagesInput.value = ''
+  if (author.length < 3 || author.length > 50) {
+    return;
+  }
 
-    modal.style.display = 'none'
-    loadCards()
-})
+  if (isNaN(pages) || pages < 25 || pages > 5000) {
+    return;
+  }
+
+  errorMessage.textContent = '';
+
+  const newBook = new Book(title, author, pages, false);
+  books.push(newBook);
+
+  titleInput.value = '';
+  authorInput.value = '';
+  pagesInput.value = '';
+
+  modal.style.display = 'none';
+  loadCards();
+});
 
 function loadCards() {
-    const bookCards = books.map(book => `
+  const bookCards = books
+    .map(
+      (book) => `
         <div class="bookCard" id="${book.id}">
             <div class="cardHead">
                 <h2>${book.title}</h2>
@@ -60,26 +75,28 @@ function loadCards() {
             </div>
             <h3>Wrote by ${book.author}</h3>
             <h3>it has ${book.pages} pages</h3>
-            <button class='read-${book.read}' onClick='{toggleRead(${book.id}, ${book.read})}' >${book.read ? 'read' : 'not read'}</button>
+            <button class='read-${book.read}' onClick='{toggleRead(${
+        book.id
+      }, ${book.read})}' >${book.read ? "read" : "not read"}</button>
         </div>
-    `).join('')
-    content.innerHTML = bookCards
+    `
+    )
+    .join("")
+  content.innerHTML = bookCards
 }
 
 function toggleRead(bookId, read) {
-    const book = books.find(book => book.id === bookId)
+  const book = books.find((book) => book.id === bookId)
 
+  if (book.read) book.read = false
+  else book.read = true
 
-    if (book.read) book.read = false
-    else book.read = true
-
-    loadCards()
-
+  loadCards()
 }
 
 function deleteCard(bookId) {
-    const newBooks = books.filter(book => book.id !== parseInt(bookId))
-    books = newBooks
+  const newBooks = books.filter((book) => book.id !== parseInt(bookId))
+  books = newBooks
 
-    loadCards()
+  loadCards()
 }
